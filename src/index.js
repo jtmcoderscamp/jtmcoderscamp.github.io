7c4d1ce35ad866629d6dfb9d0fe2f1de671d3661
Damian Borowy <tymooteuusz@gmail.com>
2019-08-30 21:54:59 +0200

    GPSLocation now throws an error instead of returning empty object

--- src/index.js
+++ src/index.js
@@ -2,11 +2,4 @@
 
 // plik inicjalizacyjny aplikacji
 
-async function foo() {
-    let x = new GpsApiLocationSource();
-    let y = await x.getLocation();
-
-    console.log(y);
-}
-
-foo();
+console.log("...");
--- src/locationSource/GpsApiLocationSource.js
+++ src/locationSource/GpsApiLocationSource.js
@@ -1,15 +1,5 @@
 export default class GpsApiLocationSource {
     async getLocation() {
-        try {
-            return await this._safeGetLocation();
-        } catch (e) {
-            this._logError(e);
-        }
-
-        return {};
-    }
-
-    async _safeGetLocation() {
         let {coords} = await this._getCurrentPosition();
         let {latitude, longitude} = coords;
 
@@ -24,22 +14,5 @@ export default class GpsApiLocationSource {
         }
         
         return new Promise(expr);
-    }
-
-    _logError(error) {
-        switch(error.code) {
-            case error.PERMISSION_DENIED:
-                console.error("User denied the request for Geolocation.");
-                break;
-            case error.POSITION_UNAVAILABLE:
-                console.error("Location information is unavailable.");
-                break;
-            case error.TIMEOUT:
-                console.error("The request to get user location timed out.");
-                break;
-            case error.UNKNOWN_ERROR:
-                console.error("An unknown error occurred.");
-                break;
-        }
     }
 }
