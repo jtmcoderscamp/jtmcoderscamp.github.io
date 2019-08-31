27b7e80d894deeb6d5429e7c1353b2236cf76483
Eliastion <zajackic@wp.pl>
2019-08-31 07:24:36 +0200

    polish and problem handling (throwingerrors)

--- src/locationSource/IpBasedLocationSource.js
+++ src/locationSource/IpBasedLocationSource.js
@@ -19,21 +19,44 @@ export default class IpBasedLocationSource{
     }
 
     async checkLocation(){
-        let response = await fetch(this._apiUrl);
-        if(!response.ok){
-            throw response.status+" "+response.statusText;
+        let response = {};
+        try{
+            response = await fetch(this._apiUrl);
+        }catch (err){
+            throw new Error(`Error when trying to access ${this._apiUrl}: ${err}`)
         }
+
+        if(!response.ok){
+            throw new Error(`${this._apiUrl} responded with ${response.status}: ${response.statusText}`);
+        }
+
         let jsonResponse = await response.json();
-        let coordinates = [];
+
+        let coordinates = {};
+
         if(this._coordinatesFieldMode==="singleField"){
             let location = resolvePath(jsonResponse, this._locationFieldPath);
-            coordinates = location.split(this._locationFieldSeparator);
+            let coordinatesArray = location.split(this._locationFieldSeparator);
+            if(coordinatesArray.length===2){
+                coordinates = {latitude: coordinatesArray[0], longitude: coordinatesArray[1]};
+            }
         }
         else if(this._coordinatesFieldMode==="doubleField"){
-            coordinates[0] = resolvePath(jsonResponse, this._latitudeFieldPath);
-            coordinates[1] = resolvePath(jsonResponse, this._longitudeFieldPath);
+            coordinates.latitude = resolvePath(jsonResponse, this._latitudeFieldPath);
+            coordinates.longitude = resolvePath(jsonResponse, this._longitudeFieldPath);
         }
-        else throw "Invalid object state";
-        return coordinates;
+        else throw new Error("Invalid coordinate interpreter mode");
+
+        if(coordinates 
+            && typeof coordinates.latitude === "number" 
+            && typeof coordinates.longitude === "number"
+            && coordinates.latitude <= 90
+            && coordinates.latitude >= -90
+            && coordinates.longitude <= 180
+            && coordinates.longitude >= -180){
+
+            return coordinates;
+        }
+        else throw new Error(`Invalid coordinates received from ${this._apiUrl} or parsing error occured`);
     }
 }
