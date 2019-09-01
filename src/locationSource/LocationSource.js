f2d22eb61bf8c9de354d1c131d009c6d807a4a42
Damian Borowy <tymooteuusz@gmail.com>
2019-09-01 22:54:28 +0200

    Fixed LocationSource

--- src/locationSource/LocationSource.js
+++ src/locationSource/LocationSource.js
@@ -7,12 +7,12 @@ export default class LocationSource {
 
         if (!location) location = await this._getIPLocation();
 
-        if (!location) return location;
+        if (location) return location;
         else throw "Unable to determine the location"
     }
 
     async _getGpsLocation() {
-        let location = {};
+        let location;
 
         try {
             location = await this._getGpsLocationWithTimeout();
@@ -33,7 +33,7 @@ export default class LocationSource {
     }
 
     async _getIPLocation() {
-        let location = {};
+        let location;
 
         try {
             location = await this._getIPLocationWithTimeout();
@@ -47,13 +47,11 @@ export default class LocationSource {
     async _getIPLocationWithTimeout() {
         return await Promise.race([
             new IpBasedLocationSource().checkLocation(),
-            new IpBasedLocationSource("http://ip-api.com/json", "lat", "lon").checkLocation(),
-            new IpBasedLocationSource("https://api.ipdata.co?api-key=5102d8d1676795d31695bc82a1fb00c8c51ad6d0d03326b039f39f60").checkLocation(),
-            this._timeout(20000)
+            new IpBasedLocationSource("https://api.ipdata.co?api-key=5102d8d1676795d31695bc82a1fb00c8c51ad6d0d03326b039f39f60").checkLocation()
         ]);
     }
 
     async _timeout(time) {
-        return new Promise(resolve => setTimeout(resolve({}), time));
+        return new Promise(resolve => setTimeout(resolve, time));
     }
 }
