806f27f434fa0669483e9d5dace35e25b001d3df
Damian Borowy <tymooteuusz@gmail.com>
2019-09-04 12:22:46 +0200

    Simplified code

--- src/locationSource/LocationSource.js
+++ src/locationSource/LocationSource.js
@@ -20,48 +20,21 @@ export default class LocationSource {
     }
 
     async checkLocation() {
-        let location = await this._getGpsLocation();
-
-        if (!location) location = await this._getIPLocation();
-
-        if (location) return location;
-        else throw "Unable to determine the location";
+        try {
+            return await this._getGpsLocation();
+        } catch (e) {
+            return await this._getIPLocation();
+        }
     }
 
     async _getGpsLocation() {
-        let location;
-
-        try {
-            location = await this._getGpsLocationWithTimeout();
-        } catch (e) {
-            console.error(e);
-        }
-
-        return location;
-    }
-
-    async _getGpsLocationWithTimeout() {
-        let gps = new GpsApiLocationSource();
-
         return this._raceWithoutRejection([
-            gps.checkLocation(),
+            new GpsApiLocationSource().checkLocation(),
             this._timeout(LocationSource.GPS_TIMEOUT)
         ]);
     }
 
     async _getIPLocation() {
-        let location;
-
-        try {
-            location = await this._getIPLocationWithTimeout();
-        } catch (e) {
-            console.error(e);
-        }
-
-        return location;
-    }
-
-    async _getIPLocationWithTimeout() {
         return this._raceWithoutRejection([
             new IpBasedLocationSource().checkLocation(),
             ...this._mapToPromises(),
