35ff2bc9f54feb7f00a4fe8eaf7dbdc7c12c8ed0
Damian Borowy <tymooteuusz@gmail.com>
2019-09-08 17:08:49 +0200

    Added main App class

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import LocationSource from "./locationSource/LocationSource";
+import WeatherSource from "./weatherSource/WeatherSource";
+import WeatherPresentation from "./userInterface/WeatherPresentation";
+
+export default class App {
+    static get WEATHER_CONTAINER() {
+        return document.querySelector('.main-page');
+    }
+
+    async start() {
+        const weather = await this._checkWeather();
+
+        this._addEventListenerAtDocumentReady(weather, this._linkWeatherWithDiv);
+    }
+
+    _addEventListenerAtDocumentReady(weather, callback) {
+        if (document.readyState === "complete" || document.readyState === "interactive") {
+            setTimeout(callback(weather), 1);
+        } else {
+            document.addEventListener("DOMContentLoaded", callback(weather));
+        }
+    }
+
+    _linkWeatherWithDiv(weather) {
+        const weatherPresentation = new WeatherPresentation(App.WEATHER_CONTAINER, weather);
+    }
+
+    async _checkWeather() {
+        const weatherAPI = new WeatherSource('http://api.openweathermap.org/data/2.5/','953349aaa2569f9cd4821f8c2ffda23a');
+        const location = await this._checkLocation();
+
+        return weatherAPI.getCurrentWeather(location);
+    }
+
+    async _checkLocation() {
+        const locationSource = new LocationSource();
+
+        return locationSource.checkLocation();
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,28 +1,4 @@
-﻿import GpsApiLocationSource from "./locationSource/GpsApiLocationSource";
-import OpenWeatherMapApi from './weatherSource/WeatherSource'
-import WeatherPresentation from "./userInterface/WeatherPresentation";
-import Weather from "./model/Weather";
-import LocationSource from "./locationSource/LocationSource";
-import IpBasedLocationSource from "./locationSource/IpBasedLocationSource";
+﻿import App from "./App";
 
-console.log("...");
-
-var weatherAPI = new OpenWeatherMapApi('http://api.openweathermap.org/data/2.5/','953349aaa2569f9cd4821f8c2ffda23a')
-let locationSource = new IpBasedLocationSource();
-let node = document.getElementById("root");
-
-let displayLocalWeather = async function() {
-    let location = await locationSource.checkLocation();
-    //let location = {latitude: 1.0, longitude: 1.0};
-    console.log(location);
-    let weather = await weatherAPI.getCurrentWeather(location);
-    //let weather = new Weather(25.13);
-    console.log(weather);
-    let test = new WeatherPresentation(node,weather);
-}
-
-displayLocalWeather();
-    
-    
-
-//let test = new WeatherPresentation(node, new Weather(10,20,Weather.PRECIPITATION_TYPE.SNOW,5,0,3,[]));
+const app = new App();
+app.start();
