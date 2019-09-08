3b7542d7f7abc7bd58a4553b3b83477b70b3487d
Damian Borowy <tymooteuusz@gmail.com>
2019-09-08 19:07:33 +0200

    Made App class ready for handling input and updating weather

--- src/App.js
+++ src/App.js
@@ -7,13 +7,19 @@ export default class App {
         return document.querySelector('.weather-presentation-container');
     }
 
+    static get INPUT_FIELD() {
+        return document.querySelector(
+            // TODO:: podpiąć jakoś formularz
+        );
+    }
+
     async start() {
         const weather = await this._checkWeather();
 
-        this._addEventListenerAtDocumentReady(weather, this._linkWeatherWithDiv);
+        this._addEventListenerAtDocumentReady(weather, this._onDocumentReady);
     }
 
-    _addEventListenerAtDocumentReady(weather, callback) {
+    _addEventListenerOnDocumentReady(weather, callback) {
         if (document.readyState === "complete" || document.readyState === "interactive") {
             setTimeout(callback(weather), 1);
         } else {
@@ -21,8 +27,19 @@ export default class App {
         }
     }
 
-    _linkWeatherWithDiv(weather) {
+    _onDocumentReady(weather) {
         const weatherPresentation = new WeatherPresentation(App.WEATHER_CONTAINER, weather);
+
+        this._addInputHandler(weatherPresentation);
+    }
+
+    _addInputHandler(weatherPresentation) {
+        // TODO:: ogarnąć jakoś pobieranie miasta z tego co zostało wpisane
+        // TODO:: a potem zaktualizować pogodę przekazując aktualną pogodę
+        // TODO:: (obiekt typu Weather) do weatherPresentation, np.:
+        
+        // let weather = {};
+        // weatherPresentation.changeWeather(weather);
     }
 
     async _checkWeather() {
